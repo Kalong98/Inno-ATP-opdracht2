96f521143a56eeaaf7224af6ea9a84e97cbee657
Kalong98 <68122498+Kalong98@users.noreply.github.com>
2023-09-21 16:44:56 +0200

    first commit

---
+++ decorators.py
@@ -0,0 +1,28 @@
+import time
+
+commands = {"configure", "measure_temp", "measure_humidity", "commando1", "commando2"}
+
+def check_command(f):
+    def inner(self, command):
+        if command in commands:
+            print(f'command: "{command}" recognized')
+            f(self, command)
+        else:
+            print(f'command: "{command}" not recognized')
+    return inner
+
+def log_command(f):
+    def inner(self, command):
+        f(self, command)
+        print(f"Command has been send") 
+        return
+    return inner
+
+def measure_rate(f):
+    def inner(self):
+        start_time = time.time()
+        f(self)
+        duration = time.time() - start_time
+        print(f'Rate of measurements: {1/duration}Hz')
+        return
+    return inner
---
+++ interface.py
@@ -0,0 +1,59 @@
+# import serial
+from decorators import *
+import time
+
+class ISHT35:
+    serial_connection = None
+    serialPort = None
+
+    def __init__(self, serialPort):
+        self.serialPort = serialPort
+
+    
+    def connect(self): # Mock-up functie voor serial connectie:
+        # try:
+        #     self.serial_connection = serial.Connect(self.serial_port)
+        #     return True
+        # except serial.SerialException as e:
+        #     print(f"Failed to connect: {e}")
+        #     return False
+        self.serial_connection = True
+        print("Connected")
+        pass
+
+    def disconnect(self): # Mock-up functie voor serial connectie:
+        # if self.serial_connection:
+        #     self.serial_connection.disconnect()
+        if self.serial_connection:
+            self.serial_connection = False
+            print ("Disconnected")
+        pass
+    
+    @check_command
+    @log_command
+    def send_command(self, command): # Mock-up functie voor interface:
+
+        if self.serial_connection:
+            print(f'sending command: "{command}"')
+            # some send logic
+            pass
+    @measure_rate
+    def get_humidity(self): # Mock-up functie voor interface:
+        # if self.serial_connection:
+        #     return self.serial_connection.read()
+        if self.serial_connection:
+            self.send_command("measure_humidity")
+            # return self.serial_connection.read()
+            time.sleep(0.025) # simulating read time
+            pass
+
+SHT35 = ISHT35("COM1")
+SHT35.connect()
+print()
+SHT35.send_command("configure")
+print()
+SHT35.send_command("unknown_command")
+print()
+SHT35.get_humidity()
+print()
+SHT35.disconnect()
